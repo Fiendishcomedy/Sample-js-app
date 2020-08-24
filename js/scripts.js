4b6e8b7c07aa9a89049e6c686d4af71ba6473646
Fiendishcomedy <68972189+Fiendishcomedy@users.noreply.github.com>
2020-08-24 11:46:06 -0700

    your comment

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,18 @@
 var pokemonList = [
     {
-        name: "mewtwo",
-        height: 7,
+        name: "Mewtwo(height:7)",
         types: ['psychic']
     },
     {
-        name: "Charizard",
-        height: 15,
+        name: "Charizard(height:15)"
         types: ['fire', 'flying']
     },
     {
-        name: "Haunter",
-        height: 4,
+        name: "Haunter(height:4)"
         types: ['ghost', 'poison']
     },
 ];
 console.log(pokemonList)
-document.write(pokemonList.name)
+
+
+
