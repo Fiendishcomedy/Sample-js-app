9d9b15231100f77f46ee6e29e4bfabfb8c384946
Fiendishcomedy <68972189+Fiendishcomedy@users.noreply.github.com>
2020-10-16 15:12:37 -0700

    your comment

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,7 @@
 var pokemonRepository = (function () {
-  var pokemonList = [
-    {
-      name: "Mewtwo",
-      height: 7,
-      types: ["Psychic"],
-      weight: 70,
-    },
-    {
-      name: "Charizard",
-      height: 15,
-      types: ["Fire", "Flying"],
-      weight: 300,
-    },
-    {
-      name: "Haunter",
-      height: 4,
-      types: ["Ghost", "Poison"],
-      weight: 50,
-    },
-    {
-      name: "Pikachu",
-      height: 2,
-      types: ["Electric"],
-      weight: 2,
-    },
-    {
-      name: "Jigglypuff",
-      height: 1,
-      types: ["Fairy"],
-      weight: 1,
-    },
-    {
-      name: "Onix",
-      height: 28,
-      types: ["Rock"],
-      weight: 460,
-    },
-  ];
+  var pokemonList = [];
+  var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
   function add(pokemon) {
     pokemonList.push(pokemon);
   }
@@ -55,60 +20,90 @@ function addListItem (pokemon){
     showDetails(pokemon)
   })
 } 
-function showDetails(pokemon){
-  console.log(pokemon)
-
+function showDetails(pokemon) {
+  loadDetails(pokemon).then(function () {
+    console.log(pokemon);
+  });
 }
-
+function loadList() {
+  return fetch(apiUrl).then(function (response) {
+    return response.json();
+  }).then(function (json) {
+    json.results.forEach(function (item) {
+      var pokemon = {
+        name: item.name,
+        detailsUrl: item.url
+      };
+      add(pokemon);
+    });
+  }).catch(function (e) {
+    console.error(e);
+  })
+}
+function loadDetails(item) {
+  var url = item.detailsUrl;
+  return fetch(url).then(function (response) {
+    return response.json();
+  }).then(function (details) {
+    // Now we add the details to the item
+    item.imageUrl = details.sprites.front_default;
+    item.height = details.height;
+    item.types = details.types;
+  }).catch(function (e) {
+    console.error(e);
+  });
+}
   return {
+    loadList: loadList,
     add: add,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
+    loadDetails: loadDetails
   };
 })();
-pokemonRepository.add({
-  name: "Pikachu",
-  height: 0.3,
-  types: ["Electric"],
-  weight: 10,
-});
-pokemonRepository.getAll().forEach(function (pokemon) {
-  var size;
-  if (pokemon.height >= 10) {
-    size = " (WOWZERS!)";
-  } else if (pokemon.height > 5 && pokemon.height < 10) {
-    size = " medium monster";
-  } else {
-    size = " small monster ";
-  }
-  var color;
-
-  pokemon.types.forEach(function (itemtype) {
-    if (itemtype == "Psychic") {
-      color = '<span style="color:purple;"> ';
-    } else if (itemtype == "Fire") {
-      color = '<span style="color:red;"> ';
-    } else if (itemtype == "Ghost") {
-      color = '<span style="color:dodgerblue;"> ';
-    } else if (itemtype == "Electric") {
-      color = '<span style="color:orange;">';
-    } else if (itemtype == "Fairy") {
-      color = '<span style="color:magenta;">';
-    } else if (itemtype == "Rock") {
-      color = '<span style="color:brown;">';
-    }
+pokemonRepository.loadList().then(function() {
+  // Now the data is loaded!
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
   });
+});
+// pokemonRepository.getAll().forEach(function (pokemon) {
+//   var size;
+//   if (pokemon.height >= 10) {
+//     size = " (WOWZERS!)";
+//   } else if (pokemon.height > 5 && pokemon.height < 10) {
+//     size = " medium monster";
+//   } else {
+//     size = " small monster ";
+//   }
+//   var color;
 
-  var weight;
-  if (pokemon.weight > 100) {
-    weight = "Heavy Pokemon";
-  } else if (pokemon.weight > 10 && pokemon.weight < 100) {
-    weight = "Average Pokemon weight";
-  } else {
-    weight = "Light Weight Pokemon";
-  }
+//   pokemon.types.forEach(function (itemtype) {
+//     if (itemtype == "Psychic") {
+//       color = '<span style="color:purple;"> ';
+//     } else if (itemtype == "Fire") {
+//       color = '<span style="color:red;"> ';
+//     } else if (itemtype == "Ghost") {
+//       color = '<span style="color:dodgerblue;"> ';
+//     } else if (itemtype == "Electric") {
+//       color = '<span style="color:orange;">';
+//     } else if (itemtype == "Fairy") {
+//       color = '<span style="color:magenta;">';
+//     } else if (itemtype == "Rock") {
+//       color = '<span style="color:brown;">';
+//     }
+//   });
 
-  pokemonRepository.addListItem(pokemon)
+//   var weight;
+//   if (pokemon.weight > 100) {
+//     weight = "Heavy Pokemon";
+//   } else if (pokemon.weight > 10 && pokemon.weight < 100) {
+//     weight = "Average Pokemon weight";
+//   } else {
+//     weight = "Light Weight Pokemon";
+//   }
+
+//   pokemonRepository.addListItem(pokemon)
   // document.write(
   //   '<div class="box">' +
   //     pokemon.name +
@@ -127,7 +122,7 @@ pokemonRepository.getAll().forEach(function (pokemon) {
   //     pokemon.types +
   //     "</div>"
   // );
-});
+// });
 
 
 
